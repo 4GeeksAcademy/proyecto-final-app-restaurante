1e078c7737d9883c0533dbb09ab3cef83f460fec
Kelvin Ruiz <kelvinruiz.developer@gmail.com>
2023-06-30 18:17:52 -0800

    MOD: home, delete RIGO

--- src/front/js/component/searchBar.js
+++ src/front/js/component/searchBar.js
@@ -10,19 +10,21 @@ export const SearchBar = () => {
 
     return (
         <div className="container">
-            <div className="row">
-                <div className="col">
-                    <h2>Hoy como con:</h2>
+            <form className="">
+                <div className="row">
+                    <div className="col">
+                        <h2>Hoy como con:</h2>
+                    </div>
+                    <div className="input-group col">
+                        <input type="text" className="form-control" placeholder="Buscar por tipo de comida o restaurante..." aria-label="Buscar" aria-describedby="button-addon2" />
+                    </div>
                 </div>
-                <div className="input-group col">
-                    <input type="text" className="form-control" placeholder="Buscar por tipo de comida o restaurante..." aria-label="Buscar" aria-describedby="button-addon2" />
-                </div>
-            </div>
 
-            <div className="input-group">
-                <input type="text" className="form-control" placeholder="Buscar por tipo de comida o restaurante..." aria-label="Buscar" aria-describedby="button-addon2" />
-                <button className="btn btn-primary" type="button" id="button-addon2"><FaSearch /></button>
-            </div>
+                <div className="input-group">
+                    <input type="text" className="form-control" placeholder="Buscar por tipo de comida o restaurante..." aria-label="Buscar" aria-describedby="button-addon2" />
+                    <button className="btn btn-primary" type="button" id="button-addon2"><FaSearch /></button>
+                </div>
+            </form>
         </div>
     );
 };
