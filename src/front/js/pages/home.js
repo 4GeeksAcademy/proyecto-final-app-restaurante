85855814b8c1c38b23d394e282d8ebaba7e046a5
LuisMarin-dev <luis.marinprog@gmail.com>
2023-07-02 19:10:02 -0400

    fixed branch issues, created the login form and fixed positioning and spacing issues within the component

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -2,13 +2,14 @@ import React, { useContext } from "react";
 import { Context } from "../store/appContext";
 import rigoImageUrl from "../../img/rigo-baby.jpg";
 import "../../styles/home.css";
+import Login from "../component/Login"
 
 export const Home = () => {
 	const { store, actions } = useContext(Context);
 
 	return (
 		<div className="text-center mt-5">
-
+			<Login />
 		</div>
 	);
 };
