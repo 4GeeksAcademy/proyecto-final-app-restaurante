3ce850c10e1d7457b00a86430bc0cfa25e5b1716
LuisMarin-dev <luis.marinprog@gmail.com>
2023-07-04 03:24:58 -0400

    added a try-catch to perform the login in the handleLogin function. Commented it just in case.

--- src/api/routes.py
+++ src/api/routes.py
@@ -107,6 +107,12 @@ def login():
     if None in [email, password]:
         return jsonify({'message': "User dict has a wrong property"}), 400
 
+    # 7-4-23 2:24
+    if email is None:
+        return jsonify({"message": "Missing email parameter"})
+    if password is None:
+        return jsonify({"message": "Missing password parameter"})
+
     user = User.query.filter_by(email=email).one_or_none()
     user_salt = user.salt
     user_role = user.role.value
@@ -115,5 +121,8 @@ def login():
     if check_password(user_password, password, user_salt):
         token = create_access_token(identity=user.name, expires_delta=False)
         return jsonify({'role': user_role, 'token': token}), 200
+    # 7-4-23 2:24
+    else:
+        return jsonify({"message": "bad credentials"}), 400
 
     return jsonify({'message': 'Wrong credentials'}), 400
