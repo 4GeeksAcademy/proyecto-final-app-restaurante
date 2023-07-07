f6f683dc92597f73939a9495c34c0478c6a7da2d
andlicon <andresjzabalac@hotmail.com>
2023-07-07 12:13:38 -0400

    ADD: searchBar is a controlled component

--- src/front/js/component/searchBar.js
+++ src/front/js/component/searchBar.js
@@ -4,28 +4,20 @@ import { FaSearch } from 'react-icons/fa';
 import { Context } from "../store/appContext";
 import '../../styles/searchBar.css';
 
+const initialValue = {
+    budget: 0,
+    food: ''
+}
+
 export const SearchBar = () => {
+    const [search, setSearch] = useState(initialValue)
 
-    // const { store, actions } = useContext(Context)
-
-    // const [search, setSearch] = useState({})
-
-    // const handleOnChange = ({ target }) => {
-    //     setSearch({
-    //         ...search,
-    //         [target.name]: target.value
-    //     })
-    // }
-
-    // const handleSubmit = () => {
-    //     //actions.foodSearch(search)
-    // }
-
-    /*
-        onChange={handleOnChange}
-        value={search.food}
-        value={search.budget}
-    */
+    const handleOnChange = ({ target }) => {
+        setSearch({
+            ...search,
+            [target.name]: target.value
+        })
+    }
 
     return (
         <div className="container searchBar">
@@ -36,10 +28,12 @@ export const SearchBar = () => {
                     </label>
                     <input
                         type="text"
-                        id="budget"
                         className="form-control d-inline w-25 border border-dark searchBar--border-bottom "
                         placeholder="$$$"
                         name="budget" /*budget = presupuesto*/
+                        id="budget"
+                        onChange={handleOnChange}
+                        value={search.budget}
 
                     />
                     <h4 className="d-inline ms-2">$</h4>
@@ -51,7 +45,10 @@ export const SearchBar = () => {
                         placeholder="Buscar por tipo de comida o restaurante..."
                         aria-label="Buscar"
                         aria-describedby="button-addon"
+                        id="food"
                         name="food"
+                        onChange={handleOnChange}
+                        value={search.food}
                     />
                     <button className="searchBar__submit btn btn-primary" type="submit" id="button-addon2"><FaSearch /></button>
                 </div>
