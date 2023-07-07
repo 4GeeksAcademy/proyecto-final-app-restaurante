3666d905b5ec4c7ec9ccade4bf6430fe9e3570d7
Juan Ramos <jars4u2@gmail.com>
2023-07-07 19:04:58 -0400

    login and register links done

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "../../styles/index.css"
+import ComeconLogo from "../../img/comecon-final-2.jpg"
 
 export const Navbar = () => {
 	return (
 		<>
 			<nav className="navbar">
 				<div className="container">
+					<img src={ComeconLogo}></img>
 					<h1 className="text-light"> Comecon </h1>
 					{/* <Link to="/">
 						<span className="navbar-brand mb-0 h1">React Boilerplate</span>
@@ -17,8 +19,8 @@ export const Navbar = () => {
 						</Link>
 					</div> */}
 					<div className="d-flex">
-						<button className="btn btn-light m-1">Login</button>
-						<button className="btn btn-light m-1">Signup</button>
+						<Link to = "/login"><button className="btn btn-light m-1">Login</button></Link>
+						<Link to = "/register"><button className="btn btn-light m-1">Signup</button></Link>
 					</div>
 				</div>
 			</nav>
