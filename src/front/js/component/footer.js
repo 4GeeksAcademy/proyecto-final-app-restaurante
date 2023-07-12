3dbc1a22845782a7adbcaf94c3af99fca34569d3
Juan Ramos <jars4u2@gmail.com>
2023-07-12 17:26:38 -0400

    style home, body bg and css issues

--- src/front/js/component/footer.js
+++ src/front/js/component/footer.js
@@ -1,9 +1,9 @@
 import React, { Component } from "react";
 
 export const Footer = () => (
-	<footer className="footer mt-auto py-3 text-center ">
+	<footer className="footer mt-auto py-3 text-center text-white">
 		<p className="">
-			Comecon.com
+			Comecon.com 2023. All rights reserved
 		</p>
 	</footer>
 );
