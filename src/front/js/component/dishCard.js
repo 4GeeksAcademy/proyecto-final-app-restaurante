9517d6bfeb7fa4ef8a2e0110d0625bbe508e9491
Juan Ramos <jars4u2@gmail.com>
2023-07-18 12:47:05 -0400

    price placed at the end of dishcard

--- src/front/js/component/dishCard.js
+++ src/front/js/component/dishCard.js
@@ -22,64 +22,23 @@ export const DishCard = ({ key, dish }) => {
                                 <Link to={`/restaurant/${dish.restaurant_id}`} className="">{dish.restaurant_name}</Link>
                                 <p className="card-text">{description}</p>
                             </div>
-                            <div className="d-flex justify-content-between align-items-center">
                                 {location.pathname === '/restaurant/menu' && (
+                            <div className="d-flex justify-content-between align-items-center">
                                     <div className="btns-container">
                                         <button className="btn btn-primary me-3">Editar</button>
                                         <button className="btn btn-danger" onClick={() => actions.deleteDish(dish.id)}>Borrar</button>
-                                    </div>)}
-                                <p className="fs-1 text-center"><strong>{`${price}$`}</strong></p>
-                            </div>
+                                    </div>
+                                <div className="fs-1 text-end"><strong>{`${price}$`}</strong></div>
+                                </div>
+                                )}
+                                {location.pathname === '/' && (
+                                    <div className="fs-1 text-end"><strong>{`${price}$`}</strong></div>
+                                )}
+
                         </div>
                     </div>
                 </div>
             </div>
-
-            {/* <div className="card">
-                <img src={image} className="card-img-top" alt={`${name} image`} />
-                <div className="card-body">
-                    <h5 className="card-title">{name}</h5>
-                    <p className="card-text">{restaurant_name}</p>
-                    <p className="card-text">{description}</p>
-                    <div className="d-flex mx-2 justify-content-end">
-                        <button className="btn btn-primary mx-4">Editar</button>
-                        <button className="btn btn-danger" onClick={() => actions.deleteDish(dish.id)}>Borrar</button>
-                        <strong>{`${price}$`}</strong>
-                    </div>
-                </div>
-            </div> */}
-
-
-
-            {/* <div className="card mb-3 dishcard-size">
-                <div className="row g-0">
-                    <div className="col-md-4 text-center">
-                        <img src={dish.image_url}
-                            className="img-fluid rounded-start dishcard-image"
-                            alt={`${dish.name} image`}
-                        />
-                    </div>
-                    <div className="col-md-8">
-                        <div className="card-body row">
-                            <div className="col-9">
-                                <Link to={`/restaurant/${dish.restaurant_id}`} className="">{dish.restaurant_name}</Link>
-                                <p className="">{dish.name}</p>
-                                <p className="">
-                                    {dish.description}
-                                </p>
-                            </div>
-                            <div className="col-3">
-                                <h5 className="card-title text-center"><strong>{`${price}$`}</strong></h5>
-                            </div>
-
-                        </div>
-                    </div>
-                </div>
-                <div className="d-flex mx-2 justify-content-end">
-                    <button className="btn btn-primary mx-4">Editar</button>
-                    <button className="btn btn-danger" onClick={() => actions.deleteDish(dish.id)}>Borrar</button>
-                </div>
-            </div> */}
         </div>
     );
 };
