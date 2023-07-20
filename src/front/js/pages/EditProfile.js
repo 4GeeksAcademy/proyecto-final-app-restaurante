593833a1ca78e4460a57b6fad32e6054c5cf5ea7
andlicon <andresjzabalac@hotmail.com>
2023-07-20 09:56:15 -0400

    RN: useless elements

--- src/front/js/pages/EditProfile.js
+++ src/front/js/pages/EditProfile.js
@@ -1,13 +1,13 @@
-import React, {useContext, useEffect, useState} from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { Context } from "../store/appContext";
 import "../../styles/editProfile.css"
 
 
 const initialValues = {
     name: "",
-    rif: "",
+    // rif: "",
     phone: "",
-    email: "",
+    // email: "",
     location: "",
     description: "",
     password: "",
@@ -30,9 +30,9 @@ const EditProfile = () => {
         const formData = new FormData();
 
         formData.append("restaurantName", restaurant.name);
-        formData.append("restaurantRif", restaurant.rif);
+        // formData.append("restaurantRif", restaurant.rif);
         formData.append("restaurantPhone", restaurant.phone);
-        formData.append("userEmail", restaurant.email);
+        // formData.append("userEmail", restaurant.email);
         formData.append("restaurantLocation", restaurant.location);
         formData.append("restaurantDescription", restaurant.description);
         formData.append("userPassword", restaurant.password);
@@ -40,62 +40,62 @@ const EditProfile = () => {
         formData.append("restaurantInstagram", restaurant.instagram);
         formData.append("restaurantTwitter", restaurant.twitter);
 
+        console.log(formData);
         actions.editRestaurant(restaurant);
     }
 
     const handleChange = (event) => {
-        console.log(event.target.name)
-        setRestaurant({...restaurant,[event.target.name]:event.target.value})        
+        setRestaurant({ ...restaurant, [event.target.name]: event.target.value });
     }
 
-    return(
+    return (
         <div className="container mt-4 profileEdit-main-container">
             <div className="col-12 col-sm-9 col-md-7 col-lg-6 col-lx-5 bg-white profileEdit_form_container border">
                 <span className="w-100 bg-danger editProfile_title">Editar perfil de datos</span>
                 <form className="col-8 editProfile_form" onSubmit={handleEdit}>
                     <div className="mb-3">
-                        <label htmlFor="business-name" className="form-label">Nombre del local</label>
+                        <label htmlFor="name" className="form-label">Nombre del local</label>
                         <input type="text" className="form-control border" id="name" aria-describedby="emailHelp" name="name" value={restaurant.name} onChange={handleChange} />
                     </div>
-                    <div className="mb-3">
+                    {/* <div className="mb-3">
                         <label htmlFor="rif-input" className="form-label">RIF del Local</label>
                         <input type="text" className="form-control border" id="rif-input" aria-describedby="emailHelp" name="rif" value={restaurant.rif} onChange={handleChange}/>
-                    </div>
+                    </div> */}
                     <div className="mb-3">
                         <label htmlFor="phone-number-input" className="form-label">Teléfono de contacto</label>
-                        <input type="text" className="form-control border" id="phone-number-input" aria-describedby="emailHelp" name="phone" value={restaurant.phone} onChange={handleChange}/>
+                        <input type="text" className="form-control border" id="phone-number-input" aria-describedby="emailHelp" name="phone" value={restaurant.phone} onChange={handleChange} />
                     </div>
-                    <div className="mb-3">
+                    {/* <div className="mb-3">
                         <label htmlFor="email-input" className="form-label">Email</label>
                         <input type="email" className="form-control border" id="email-input" aria-describedby="emailHelp" name="email" value={restaurant.email} onChange={handleChange}/>
-                    </div>
+                    </div> */}
                     <div className="mb-3">
                         <label htmlFor="location-input" className="form-label">Link de ubicación</label>
-                        <input type="text" className="form-control border" id="location-input" aria-describedby="emailHelp" name="location" value={restaurant.location} onChange={handleChange}/>
+                        <input type="text" className="form-control border" id="location-input" aria-describedby="emailHelp" name="location" value={restaurant.location} onChange={handleChange} />
                     </div>
                     <div className="mb-3">
                         <label htmlFor="description-input" className="form-label">Descripción</label>
-                        <textarea className="form-control" id="description-input" name="description" rows="3" value={restaurant.description} onChange={handleChange}/>
+                        <textarea className="form-control" id="description-input" name="description" rows="3" value={restaurant.description} onChange={handleChange} />
+                    </div>
+
+                    <div className="mb-3">
+                        <label htmlFor="facebook-input" className="form-label">Facebook</label>
+                        <input type="text" className="form-control border" id="facebook-input" aria-describedby="emailHelp" name="facebook" value={restaurant.facebook} onChange={handleChange} />
                     </div>
                     <div className="mb-3">
-                    <div className="mb-3">
-                        <label htmlFor="location-input" className="form-label">Facebook</label>
-                        <input type="text" className="form-control border" id="facebook-input" aria-describedby="emailHelp" name="facebook" value={restaurant.facebook} onChange={handleChange}/>
+                        <label htmlFor="instagram-input" className="form-label">Instagram</label>
+                        <input type="text" className="form-control border" id="instagram-input" aria-describedby="emailHelp" name="instagram" value={restaurant.instagram} onChange={handleChange} />
                     </div>
                     <div className="mb-3">
-                        <label htmlFor="location-input" className="form-label">Instagram</label>
-                        <input type="text" className="form-control border" id="instagram-input" aria-describedby="emailHelp" name="instagram" value={restaurant.instagram} onChange={handleChange}/>
+                        <label htmlFor="twitter-input" className="form-label">Twitter</label>
+                        <input type="text" className="form-control border" id="twitter-input" aria-describedby="emailHelp" name="twitter" value={restaurant.twitter} onChange={handleChange} />
                     </div>
-                    <div className="mb-3">
-                        <label htmlFor="location-input" className="form-label">Twitter</label>
-                        <input type="text" className="form-control border" id="twitter-input" aria-describedby="emailHelp" name="twitter" value={restaurant.twitter} onChange={handleChange}/>
-                    </div>
-                        <label htmlFor="password-input" className="form-label">Password</label>
-                        <input type="password" className="form-control" id="password-input" name="password" value={restaurant.password} onChange={handleChange}/>
-                    </div>
+                    <label htmlFor="password-input" className="form-label">Password</label>
+                    <input type="password" className="form-control" id="password-input" name="password" value={restaurant.password} onChange={handleChange} />
+
                     <div className="mb-3 mt-3 d-flex justify-content-center">
                         <button type="submit" className="btn btn-success bg-success col-4 login_submit_button">Actualizar</button>
-                        <button type="submit" className="btn btn-danger bg-danger col-4 login_submit_button">Cancelar</button>
+                        <button type="button" className="btn btn-danger bg-danger col-4 login_submit_button">Cancelar</button>
                     </div>
                 </form>
             </div>
